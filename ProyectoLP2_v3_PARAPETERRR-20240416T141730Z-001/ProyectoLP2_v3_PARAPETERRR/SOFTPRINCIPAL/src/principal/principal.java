4f2f952eece89fb5534d708d090de110946734e9
Alonso <Alonso@10.100.68.116>
2024-04-16 09:31:39 -0500

    pll

---
+++ ProyectoLP2_v3_PARAPETERRR-20240416T141730Z-001/ProyectoLP2_v3_PARAPETERRR/SOFTPRINCIPAL/src/principal/principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package principal;
+
+import java.util.ArrayList;
+import lothel.soft.evento.dao.EspacioDAO;
+import lothel.soft.evento.model.Espacio;
+import lothel.soft.evento.mysql.EspacioMYSQL;
+
+/**
+ *
+ * @author efeproceres
+ */
+public class principal {
+ //peterChupaCohete
+}
